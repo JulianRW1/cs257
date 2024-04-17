1ee7d3dfddb61190631994b6bf62fbd798e99260
JulianRW1 <julianrwalston@gmail.com>
2024-04-17 10:40:16 -0500

    moved createtables.py to pythonlab

---
+++ pythonlab/createtables.py
@@ -0,0 +1,30 @@
+def create_tables():
+    
+  conn = psycopg2.connect(
+      host="localhost",
+      port=5432,   
+      database="walstonj",
+      user="walstonj",
+      password="tablet995sunshine")
+
+  cur = conn.cursor()
+
+  cities = """CREATE TABLE DROP TABLE IF EXISTS cities;
+    CREATE TABLE cities (
+      code text,
+      state text,
+      pop real
+    );"""
+
+  states = """CREATE TABLE DROP TABLE IF EXISTS states;
+    CREATE TABLE states (
+      city text,
+      state text,
+      pop real,
+      lat real,
+      long real
+    );""" 
+  cur.execute(cities)
+  cur.execute(states)
+
+create_tables()
