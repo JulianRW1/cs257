55a30f5a64572a96111a4089b7b81e7762441434
JulianRW1 <julianrwalston@gmail.com>
2024-04-26 10:31:39 -0500

    connect to cities database

--- flasklab/web-app/app.py
+++ flasklab/web-app/app.py
@@ -12,9 +12,27 @@ def welcome():
 def rand():
     name = random.choice(['Judy', 'Michael', 'Randy', 'Cal', 'Lucy', 'Susan'])
     adj = random.choice(['Wise', 'Brave', 'Inevitable', 'Unconventional', 'Wretched'])
-    city = random.choice(['Seattle', 'Northfield', 'New York', 'San Fransisco', 'Houston', 'Miami', 'St. Louis'])
+    city = get_city()
     year = random.randint(1900, 2024)
     return render_template("random.html", randName = name, randAdj = adj, randCity = city, randYear = year)
+
+def get_city():
+    conn = psycopg2.connect(
+      host="localhost",
+      port=5432,   
+      database="walstonj",
+      user="walstonj",
+      password="tablet995sunshine")
+
+    cur = conn.cursor()
+
+    sql = "SELECT city FROM cities"
+
+    cur.execute( sql )
+
+    cities = cur.fetchall()
+    city = random.choice(cities)
+    return city[0]
 
 if __name__ == '__main__':
     my_port = 5131
