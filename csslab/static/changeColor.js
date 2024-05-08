9632c0585a6166a29675db1756c821e94c147af7
JulianRW1 <julianrwalston@gmail.com>
2024-05-08 11:10:50 -0500

    made color random

--- csslab/static/changeColor.js
+++ csslab/static/changeColor.js
@@ -1,5 +1,9 @@
 function changeColor() {
   the_heading = document.getElementById("textToChange");
+
+  let colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+  let color = colors[Math.floor(Math.random()*colors.length)];
   the_heading.style.color = "red";
+
   console.log("I just changed the color to: " + the_heading.style.color); 
 }
